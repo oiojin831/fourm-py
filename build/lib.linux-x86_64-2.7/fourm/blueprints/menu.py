9c4068b2e163dfc4f061cde3226f3ba4794f216e
eung Jin Lee <oiojin831@gmail.com>
2014-12-02 11:18:19 +0900

    add new gitignore with gitignore.io

--- build/lib.linux-x86_64-2.7/fourm/blueprints/menu.py
+++
@@ -1,8 +0,0 @@
-# -*- encoding: utf-8 -*-
-from flask import Blueprint, render_template
-
-blueprint = Blueprint('menu', __name__)
-
-@blueprint.route('/menu')
-def menu():
-    return render_template('menu.html')
